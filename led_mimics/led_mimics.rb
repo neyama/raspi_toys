4ada6aad39d355424393562e737dc5838f87366a
neyama <neyama@yahoo.co.jp>
2014-11-16 09:49:08 +0900

    event is defined by hash

--- led_mimics/led_mimics.rb
+++ led_mimics/led_mimics.rb
@@ -9,20 +9,20 @@ DELAY=3
 queue=Queue.new
 
 # monitor GPIO 17 pin
-PiPiper.watch :pin=>17, :trigger=>:both do |pin|
-  event=[Time.now, pin.on?]
+PiPiper.watch pin: 17, trigger: :both do |pin|
+  event={time: Time.now, state: pin.on?}
   queue << event
 end
 
 # LED control thread
 Thread.new do
   # initialize GPIO 27 pin
-  pin=PiPiper::Pin.new :pin=>27, :direction=>:out
+  pin=PiPiper::Pin.new pin: 27, direction: :out
   loop {
     event=queue.pop
 
-    t=event[0]
-    s=event[1]
+    t=event[:time]
+    s=event[:state]
 
     tts=t+DELAY-Time.now
     sleep tts if tts>0 # waits tts sec if needed
